179f455f89b8156f7941a1599d83f591fdf94864
javyer <javyermurillo1@gmail.com>
2021-09-01 17:51:03 -0600

    proyecto 3d

---
+++ index.js
@@ -0,0 +1,39 @@
+const buttonCar = document.querySelector('#buttonCar');
+const buttonBus = document.querySelector('#buttonBus')
+const buttonMartillo = document.querySelector('#buttonMartillo');
+
+const modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.modal__content--close')
+
+//Car modal
+buttonCar.addEventListener('click', () => {
+
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+
+})
+
+//Bus Modal
+buttonBus.addEventListener('click', () => {
+
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+
+})
+
+//Martillo Modal
+buttonMartillo.addEventListener('click', () => {
+
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+
+})
+
+closeModal.addEventListener('click', () => {
+console.log('cliked')
+    modal.classList.remove('visible')
+    modal.classList.add('hidden')
+
+})
+
+
